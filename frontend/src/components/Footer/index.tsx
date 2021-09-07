332985b2677eae95be81b51e3f5781e8b9e6ea82
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2021-09-06 23:17:40 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/Jonathas-bonfim" target="_blank" rel="noreferrer">Jônathas Bonfim</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+          Evento promovido por Jônathas: <a href="https://https://www.instagram.com/jonathas.bonfim/" target="_blank" rel="noreferrer">@jonathas.bonfim</a></small></p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
