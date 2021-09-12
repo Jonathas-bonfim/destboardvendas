c559fe140f33e643d9b2d1859bd645af58f4bc6a
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2021-09-12 17:56:54 -0300

    layout

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="container">
         <p className="text-light">App desenvolvido por <a href="https://github.com/Jonathas-bonfim" target="_blank" rel="noreferrer">Jônathas Bonfim</a></p>
         <p className="text-light"><small><strong>Semana Spring React</strong><br />
-          Evento promovido por Jônathas: <a href="https://https://www.instagram.com/jonathas.bonfim/" target="_blank" rel="noreferrer">@jonathas.bonfim</a></small></p>
+          Evento promovido por Dev Superior <br /> Insta <a href="https://https://www.instagram.com/jonathas.bonfim/" target="_blank" rel="noreferrer">@jonathas.bonfim</a></small></p>
       </div>
     </footer>
   );
